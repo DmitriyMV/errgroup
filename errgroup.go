630b0a20cb376a79b79bb816195bb65e2e1d0b89
DmitriyMV <lepage+gh@protonmail.com>
2021-05-10 00:56:27 +0300

    Minor fixes

--- errgroup.go
+++ errgroup.go
@@ -1,6 +1,7 @@
 // Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 // Package errgroup provides synchronization, error propagation, and Context
 // cancelation for groups of goroutines working on subtasks of a common task.
 package errgroup
